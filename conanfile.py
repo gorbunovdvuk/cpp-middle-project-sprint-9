09c66cd883f15638a477d4d69e5527eb10a004c3
Dmitry Gorbunov <gorbunovdvuk@gmail.com>
2025-09-10 12:58:13 +0300

    Dependencies fixes

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,10 @@ class GeometryAppConan(ConanFile):
     def requirements(self):
         self.requires("gtest/1.13.0")
         self.tool_requires("cmake/3.30.0")
+        self.requires("ogg/1.3.5")
+        self.requires("vorbis/1.3.7")
+        self.requires("flac/1.4.3")
+        self.requires("freetype/2.12.1")
     
     def layout(self):
         self.folders.source = "."
